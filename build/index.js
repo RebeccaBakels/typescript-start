4105afb8739fb34c6e3ed72a73760bf7983c4333
Rebecca Bakels <rebeccabakels@gmail.com>
2021-03-01 14:20:54 -0500

    practice

--- build/index.js
+++ build/index.js
@@ -1,2 +1,21 @@
 "use strict";
 console.log('Hello TS');
+var num1 = 5;
+var num2 = 20;
+var addNumbers = function (number1, number2) {
+    return number1 + number2;
+};
+console.log(addNumbers(num1, num2));
+var dog = {
+    name: 'Scully',
+    desc: {
+        breed: 'corgi',
+        color: 'red'
+    },
+    age: 6,
+    good: true
+};
+if (dog.desc) {
+    dog.desc.breed = 'demon';
+}
+console.log(dog);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
 console.log('Hello TS')
 
 let num1 = 5
-let num2 = 10
+let num2 = 20
 
 const addNumbers = (number1: number, number2: number) => {
     return number1 + number2
 }
 
-console.log(num1, num2)
+console.log(addNumbers(num1, num2))
+
+interface iDog {
+    name: string,
+    age: number | undefined,   //can be a number or undefined
+    good?: true                //made optional
+    desc?: {
+        breed: string,
+        color: string,
+}}
+
+let dog: iDog = {
+    name: 'Scully',
+    desc: {
+        breed: 'corgi',
+        color: 'red'
+    },
+    age: 6,
+    good: true
+}
+
+if (dog.desc){
+dog.desc.breed = 'demon'
+}
+
+console.log(dog)
